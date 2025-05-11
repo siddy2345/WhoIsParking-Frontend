d6157e47933a7125b4f2a6a9ab8fab29224f26d4
Sahid Fakiry <s.fakiry23@gmail.com>
2025-05-11 16:47:36 +0200

    feat(WIP): add pdf download for qr code

--- src/app/house/house.component.ts
+++ src/app/house/house.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit, Renderer2, signal } from '@angular/core';
 import { Observable } from 'rxjs';
 import {
-  API_BASE_URL,
   HouseClient,
   HouseViewModel,
+  IHouseViewModel,
 } from '../services/whoIsParking-api.service';
 import { CommonModule } from '@angular/common';
 import { QRCodeComponent } from 'angularx-qrcode';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-house',
@@ -16,8 +17,9 @@ import { QRCodeComponent } from 'angularx-qrcode';
 })
 export class HouseComponent implements OnInit {
   public houses$!: Observable<HouseViewModel[]>;
-  public qrCodeDataSig = signal('');
+  public qrCodeData = signal('');
   public openDialog = signal(false);
+  public selectedHouse = signal<IHouseViewModel>({});
 
   private readonly houseService = inject(HouseClient);
   private readonly renderer = inject(Renderer2);
@@ -26,8 +28,12 @@ export class HouseComponent implements OnInit {
     this.houses$ = this.houseService.housesGetAdmin();
   }
 
+  public onSelectHouse(house: HouseViewModel): void {
+    this.selectedHouse.set(house);
+  }
+
   public onGenerateQrCode(houseId: number): void {
-    this.qrCodeDataSig.set(`${houseId}`); //TODO: route of car creation with houseId as url param
+    this.qrCodeData.set(`${houseId}`); //TODO: route of car creation with houseId as url param
     this.openDialog.set(true);
     this.renderer.addClass(document.body, 'dialog-backdrop');
   }
@@ -35,5 +41,32 @@ export class HouseComponent implements OnInit {
   public onCloseDialog(): void {
     this.openDialog.set(false);
     this.renderer.removeClass(document.body, 'dialog-backdrop');
+  }
+
+  public onDownloadPdf(qrCode: QRCodeComponent): void {
+    const doc = new jsPDF('p', 'mm', 'a4');
+    const canvas = qrCode.qrcElement.nativeElement as HTMLDivElement;
+    const canvasUrl = canvas.querySelector('canvas')?.toDataURL('image/png');
+    if (!canvasUrl) {
+      console.error('canvas url undefined'); //FIXME: error handling
+    }
+
+    const imgWidth = 208;
+    const imgHeight = (canvas.clientHeight * imgWidth) / canvas.clientWidth;
+
+    doc.setFontSize(25);
+    doc.text(
+      `${this.selectedHouse().street} ${this.selectedHouse().number}`,
+      10,
+      15
+    );
+
+    doc.addImage(canvasUrl!, 'PNG', 0, 25, imgWidth, imgHeight);
+
+    doc.save(
+      `HausQrCode_${this.selectedHouse().street}_${
+        this.selectedHouse().number
+      }.pdf`
+    );
   }
 }
